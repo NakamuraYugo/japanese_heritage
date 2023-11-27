b5ae4933e909a3f3f6cc429cd2337e963d7899dc
NakamuraYugo <yayaya855@gmail.com>
2023-11-27 00:05:41 +0000

    docs: ファイルのインポートの設定をコメント解除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,8 @@ require("bootstrap");
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import 'bootstrap'
-import "../stylesheets/application";
+import "../stylesheets/application.scss";
