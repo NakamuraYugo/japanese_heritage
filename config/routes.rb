47d69ddcc8234db360259156cb5e45e4389a06be
NakamuraYugo <yayaya855@gmail.com>
2023-12-06 04:46:35 +0000

    ユーザー認証のroutesを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
 end
