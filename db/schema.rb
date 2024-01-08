614b58c81801c67fc2cf1330c39b23582490ebea
NakamuraYugo <yayaya855@gmail.com>
2024-01-08 21:00:29 +0000

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_17_034641) do
+ActiveRecord::Schema.define(version: 2024_01_05_204459) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "spot_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2023_11_17_034641) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spots", "users"
 end
