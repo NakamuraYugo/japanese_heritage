fc4a2a4da1faaf4f663b4929ab0cd17105f5421d
NakamuraYugo <yayaya855@gmail.com>
2024-02-01 13:08:28 +0000

    newアクションの設定追加、createアクションの設定追加、spot_paramsを追加

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,11 +3,30 @@ class SpotsController < ApplicationController
   end
 
   def new
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = current_user.spots.build(spot_params)
+
+    if @spot.save
+      redirect_to @spot, success: t('defaults.message.created', item: Spot.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Spot.model_name.human)
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :intrduction, :spot_image)
   end
 end
