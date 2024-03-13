9d72f57af59095bc2e1e565faf79ad463c61e4d4
NakamuraYugo <yayaya855@gmail.com>
2024-03-13 13:27:00 +0000

    feat: Spotモデルにバリデーションと画像アップロード機能の追加
    - SpotImageUploaderを使用して、複数画像のアップロード機能をSpotモデルにマウント
    - 名称（name）、説明（description）、画像（spot_images）の存在確認をするバリデーションを追加
    - 名称と説明の最大文字数をそれぞれ255文字、65535文字に制限

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,10 @@
 class Spot < ApplicationRecord
   belongs_to :user
+
+  mount_uploaders :spot_images, SpotImageUploader
+
+  validates :name, presence: true, length: {maximum: 255}
+  validates :description, presence: true, length: {maximum: 65_535}
+  validates :spot_images, presence: true
+
 end
