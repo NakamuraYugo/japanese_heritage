12e449fe54953476e981d8e8fe736c2af9c8ca3e
NakamuraYugo <yayaya855@gmail.com>
2024-03-14 07:54:28 +0000

    db: Spotモデルのスキーマを更新し、バリデーション強化とカラムの調整を実施
    - `introduction` カラムを `description` に名前変更
    - `name`、`description`、`spot_images` カラムに Not Null 制約を追加
    - `spot_image` カラムを削除し、新たに `spot_images` JSON型カラムを追加して複数画像のアップロードをサポート

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_05_204459) do
+ActiveRecord::Schema.define(version: 2024_02_27_025942) do
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.text "introduction"
-    t.string "spot_image"
+    t.string "name", null: false
+    t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.json "spot_images", null: false
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
