d1542a7483ba1aaa7463387d281ffcf7e0df5a09
NakamuraYugo <yayaya855@gmail.com>
2024-03-16 08:36:27 +0000

    feat: 複数のスポット画像選択時のプレビュー機能強化
    - 複数の画像選択に対応するため、単一の画像プレビューから複数画像プレビューへの処理を更新
    - 各選択された画像ファイルに対して、動的にimg要素を生成し、選択された画像のプレビューを表示するように改善
    - 選択された画像を格納するコンテナの内部をクリアし、新たに選択された画像のみを表示するようにすることで、ユーザーの選択操作に対するフィードバックを即時に反映
    - img要素に 'img-preview' クラスを動的に追加し、CSSで定義されたスタイルを適用することで、プレビュー画像の表示を統一的に管理

--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -1,26 +1,19 @@
-window.previewImage = function(event) {
-  const target = event.target;
+window.previewImages = function(event) {
+  const files = event.target.files;
+  const previewContainer = document.getElementById('images-preview');
 
-  // const file = target.files[0]; の行では、ユーザーが選択したファイルを取得していますが、この時点ではまだファイルの内容は読み込まれていません。ファイルの内容は、FileReader オブジェクトによって読み込まれます。
-  const file = target.files[0];
+  // 既存のプレビューをクリア
+  previewContainer.innerHTML = '';
 
-  // FileReader オブジェクトを作成します。
-  const reader = new FileReader();
-
-  // ここで、画像のプレビューを表示する処理を書く
-  // ここでは reader の onload イベントハンドラを設定しますが、この時点で実行されるわけではありません。ファイルの読み込みが完了した時に実行される関数を定義しています。ファイルの読み込みは、reader の readAsDataURL メソッドを呼び出すことで開始されます。
-  reader.onload = function (e) {
-    const previewElement = document.getElementById("preview");
-    if (previewElement) {
-      // ここで e.target.result にはreader.readAsDataURL(file)で読み込まれたファイルの内容（Base64エンコードされた画像データ）が含まれており、このデータが previewElement.src に設定されて画像が表示されます。
-      previewElement.src = e.target.result;
-    }
-  };
-
-// ここで画像ファイルが存在していたら、その画像ファイルを読み込む処理を書いています。
-// FileReader.readAsDataURL メソッドは、ファイルの内容をBase64エンコードされたURL形式で読み込み、これを例えば画像の src 属性に設定することで画像を表示する際などに使われます。
-// ファイルの読み込みが完了すると、onload イベントハンドラがトリガーされ、reader.onload で定義した関数が実行されます。
-  if (file) {
+  // 選択された各ファイルに対してプレビューを作成
+  Array.from(files).forEach(file => {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const img = document.createElement('img');
+      img.setAttribute('src', e.target.result);
+      img.classList.add('img-preview'); // CSSでスタイルを設定
+      previewContainer.appendChild(img);
+    };
     reader.readAsDataURL(file);
-  }
+  });
 }
