9b577a740c8f75c7d8688a84226931d29e6a6698
NakamuraYugo <yayaya855@gmail.com>
2024-07-18 12:37:14 +0000

    refactor(model): Spotモデルの関連付けとバリデーションの更新
    - Spotモデルにhas_many :images関連付けを追加し、dependent: :destroyオプションを設定
    - accepts_nested_attributes_for :images, allow_destroy: trueを追加して、ネストされた属性を許可
    - mount_uploaders :spot_images, SpotImageUploaderを削除
    - spot_imagesのバリデーションを削除

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :user
-
-  mount_uploaders :spot_images, SpotImageUploader
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, presence: true, length: {maximum: 255}
   validates :description, presence: true, length: {maximum: 65_535}
-  validates :spot_images, presence: true
-
 end
