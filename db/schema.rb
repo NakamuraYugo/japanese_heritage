fa2d6d78938acd99fc9c63acf320a22888a65bc6
NakamuraYugo <yayaya855@gmail.com>
2024-07-21 16:47:58 +0000

    chore(schema): スキーマの更新
    - 新しくimagesテーブルを作成し、spotsテーブルと関連付け
    - spotsテーブルから不要になったspot_imagesカラムを削除
    - usersテーブルのデフォルトnameカラム値"名無し"を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_27_025942) do
+ActiveRecord::Schema.define(version: 2024_06_11_045522) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "spot_id", null: false
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_images_on_spot_id"
+  end
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -18,7 +27,6 @@ ActiveRecord::Schema.define(version: 2024_02_27_025942) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.json "spot_images", null: false
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
@@ -30,10 +38,11 @@ ActiveRecord::Schema.define(version: 2024_02_27_025942) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", default: "名無し", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "spots"
   add_foreign_key "spots", "users"
 end
