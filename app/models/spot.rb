5ad7d0b03ada2f7532cb2c3177e5036e4ba95f96
NakamuraYugo <yayaya855@gmail.com>
2024-09-27 17:29:03 +0000

    feat: モデルの関連付け強化とバリデーションメッセージのカスタマイズ
    - inverse_ofを追加して関連付けのデータ整合性を確保
    - バリデーションメッセージをカスタム設定（presence: {message: :blank}）

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :user
-  has_many :images, dependent: :destroy
+  has_many :images, dependent: :destroy, inverse_of: :spot
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :name, presence: true, length: {maximum: 255}
-  validates :description, presence: true, length: {maximum: 65_535}
+  validates :name, presence: {message: :blank}, length: {maximum: 255}
+  validates :description, presence: {message: :blank}, length: {maximum: 65_535}
 end
