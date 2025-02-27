9b4bb702a54797aeaee3adb730c8ec1d5df362c3
NakamuraYugo <yayaya855@gmail.com>
2025-02-27 04:27:41 +0000

    feat: インクリメントによる新規画像のインデックス管理と 10 枚制限の実装
    - newImageCounter で新規画像のインデックスを管理し、既存画像との重複を回避
    - buildNewInputLi() を用意して <li> 要素を動的に生成
    - ファイル選択後はプレビューを挿入し、ラベル要素を非表示
    - 画像が 10 枚に達した場合は新規追加用の <li> を削除して制限をかける
    - adjustInputWidth など旧ロジックを整理・削除

--- app/javascript/packs/handle_files.js
+++ app/javascript/packs/handle_files.js
@@ -1,65 +1,78 @@
+let newImageCounter = 100;
+// 例: ここからスタート。既存が何枚あってもぶつからないように
+// もっと大きい値にしてもOK
+
+export function buildNewInputLi() {
+  // 新しい画像を追加する度にカウンタをインクリメント
+  newImageCounter++;
+
+  // “newImageCounter” をそのまま form-index に使う
+  return $(`
+    <li class="input" data-file-type="new">
+      <label class="upload-label">
+        <div class="upload-label__text">
+          ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード
+        </div>
+        <div class="input-area">
+          <input
+            class="hidden image_upload"
+            type="file"
+            name="spot[images_attributes][${newImageCounter}][name]"
+            data-form-index="${newImageCounter}"
+            data-file-type="new"
+          >
+        </div>
+      </label>
+    </li>
+  `);
+}
+
+// handleFiles: ファイル選択後にプレビューを作る
 export function handleFiles(files, $li, $label) {
   const $ul = $('#previews');
-  const $inputs = $ul.find('.image_upload');
+  const $inputLi = $li; // 今回ファイルを選んだ <li class="input">
 
-  $.each(files, function(index, file) {
+  $.each(files, function(i, file) {
+    // FileReader で読み込み
+    const reader = new FileReader();
+
+    // プレビュー用 DOM
     const preview = $(`
       <div class="image-preview__wrapper">
-        <img class="preview">
+        <!-- 新規追加なので data-file-type="new" とする -->
+        <img class="preview" data-file-type="new">
         <div class="image-preview__btn">
           <div class="image-preview__btn_delete"></div>
         </div>
       </div>
     `);
 
-    const append_input = $(`
-      <li class="input">
-        <label class="upload-label">
-          <div class="upload-label__text">
-            ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード
-          </div>
-          <div class="input-area">
-            <input class="hidden image_upload" type="file" name="spot[images_attributes][${index}][name]">
-          </div>
-        </label>
-      </li>
-    `);
-
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(event) {
-      $(preview).find('.preview').attr('src', event.target.result);
+    reader.onload = function(e) {
+      preview.find('img.preview').attr('src', e.target.result);
     };
 
     reader.onloadend = function() {
-      $li.append(preview);
+      // もともとの <li class="input"> 内にプレビューを差し込む
+      $inputLi.append(preview);
+
+      // もうアップロードラベルは不要なので隠す/削除する
       $label.hide();
-      $li.removeClass('input').addClass('image-preview');
+      $inputLi.removeClass('input').addClass('image-preview');
 
-      const $lis = $ul.find('.image-preview');
-
-      if ($lis.length < 10) {
-        if ($ul.find('.input').length === 0) {
-          $ul.append(append_input);
+      // 10枚制限チェック
+      const $visiblePreviews = $ul.find('.image-preview:visible');
+      const count = $visiblePreviews.length;
+      if (count < 10) {
+        // まだ10枚未満なら、新しい空の <li class="input"> があるか確認
+        if ($ul.find('li.input').length === 0) {
+          $ul.append(buildNewInputLi());
         }
+      } else {
+        // 10枚に達したら、追加できないように既存の input を消す
+        $ul.find('li.input').remove();
       }
-
-      $inputs.each(function(index, input) {
-        $(input).attr('name', `spot[images_attributes][${index}][name]`);
-      });
-
-      adjustInputWidth($lis);
     };
-  });
-}
 
-export function adjustInputWidth($lis) {
-  const $lastLi = $('#previews li:last-child');
-  if ($lis.length == 5) {
-    $lastLi.css('width', '100%');
-  } else if ($lis.length < 5) {
-    $lastLi.css('width', `calc(100% - (20% * ${$lis.length}))`);
-  } else if ($lis.length > 5 && $lis.length < 10) {
-    $lastLi.css('width', `calc(100% - (20% * (${$lis.length} - 5)))`);
-  }
+    reader.readAsDataURL(file);
+  });
 }
