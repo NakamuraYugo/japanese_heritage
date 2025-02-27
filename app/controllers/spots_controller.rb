aeca6488b238380cca3275c2c11ba82dcf37a3af
NakamuraYugo <yayaya855@gmail.com>
2025-02-27 19:28:17 +0000

    feat: SpotsController に認可処理を導入し、権限を整理
    - before_action :authenticate_user!, except: [:index, :show] でログイン必須のアクションを限定
    - set_spot メソッドで @spot を共通取得し、重複コードを削減
    - require_owner! メソッドで @spot.user_id != current_user.id の場合はroot_pathへリダイレクト
      - ログインユーザー自身の投稿のみ編集できるよう制限

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,9 @@
 class SpotsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :set_spot, only: [:show, :edit, :update]
+  before_action :require_owner!, only: [:edit, :update]
+
   def index
     @spots = Spot.all
   end
@@ -20,15 +25,12 @@ class SpotsController < ApplicationController
   end
 
   def show
-    @spot = Spot.find(params[:id])
   end
 
   def edit
-    @spot = current_user.spots.find(spot_params)
   end
 
   def update
-    @spot = current_user.spots.build(spot_params)
     if @spot.update(spot_params)
       flash[:notice] = t('defaults.message.spot_update')
       redirect_to @spot
@@ -40,6 +42,17 @@ class SpotsController < ApplicationController
 
 
   private
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
+
+  def require_owner!
+    unless @spot.user_id == current_user.id
+      flash[:alert] = t('defaults.message.spot_not_owner')
+      redirect_to root_path
+    end
+  end
 
   def spot_params
     params.require(:spot).permit(:name, :description, images_attributes: [:id, :name, :name_cache, :_destroy])
