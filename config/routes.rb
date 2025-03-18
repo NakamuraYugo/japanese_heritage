d2bb8dc5ca4aa742e03d1aa21ac3a3d7bfe0a29e
NakamuraYugo <yayaya855@gmail.com>
2025-03-18 05:22:45 +0000

    feat: Spots コントローラに削除機能を追加
    - resources :spots のルーティングに :destroy を追加
    - 投稿済みスポットを削除できるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   end
 
   resources :user_informations, only: [:show]
-  resources :spots, only: [:new, :create, :index, :show, :edit, :update]
+  resources :spots, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
